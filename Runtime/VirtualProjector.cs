f26c3ff004023d232a58cff51702e4e9cbde5561
Carl Emil Carlsen <34234131+cecarlsen@users.noreply.github.com>
2025-11-21 16:43:32 +0100

    Gizmos and non-working lens shift

--- Runtime/VirtualProjector.cs
+++ Runtime/VirtualProjector.cs
@@ -2,10 +2,16 @@
 	Copyright © Carl Emil Carlsen 2025
 	http://cec.dk
 
-	TODO: note that in URP you have to increate the cookie atlas size. Find a way to do this programmatically.
-	https://discussions.unity.com/t/cookie-atlas-resolution-and-cookie-altas-format-documentation/861822
+	This hack is limited by the URP cookie atlas max resolution which is 4096x4096.
+	In practise you can only have a single 1920x1080 projection image at full resolution.
+	https://discussions.unity.com/t/released-projector-simulator/684142/156
+
+	On lens shifting
+	- Be conservative, lens shift range varies for each projector and lens.
+	- For Panasonic, you can find some of the shift constraints here: https://docs.connect.panasonic.com/projector/calculator/tdc/index.html
 */
 
+using System;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -13,8 +19,17 @@ public class VirtualProjector : MonoBehaviour
 {
 	[SerializeField] Texture _texture;
 	[SerializeField,Range(0.1f,4f)] float _throwRatio = 0.5f;
+	[SerializeField,Range(-0.5f,0.5f)] float _horizontalLensShift = 0f;
+	[SerializeField,Range(-0.5f,0.5f)] float _verticalLensShift = 0f;
+	[SerializeField] float _brightness = 1f;
+	[SerializeField] float _range = 50f;
+	[SerializeField] Color _tint = Color.white;
 
-	[SerializeField] Shader _blitShader;
+	[Header("Gizmos")]
+	[SerializeField] bool _drawGizmosAlways = false;
+	[SerializeField] Color _gizmoColor = new Color( 1f, 1f, 1f, 0.2f );
+
+	[SerializeField,HideInInspector] Shader _blitShader;
 	[SerializeField,HideInInspector] Light _light;
 
 	RenderTexture _cookieTexture;
@@ -26,6 +41,33 @@ public class VirtualProjector : MonoBehaviour
 	bool _dirty;
 
 	static readonly string logPrepend = $"<b>[{nameof( VirtualProjector )}]</b>";
+
+	public float brightness
+	{
+		get { return _brightness; }
+		set {
+			_brightness = value > 0f ? value : 0f;
+			if( _light ) _light.intensity = _brightness;
+		}
+	}
+
+	public float range
+	{
+		get { return _range; }
+		set {
+			_range = value > 0f ? value : 0f;
+			if( _light ) _light.range = _range;
+		}
+	}
+
+	public Color tint
+	{
+		get { return _tint; }
+		set {
+			_tint = value;
+			if( _light ) _light.color = _tint;
+		}
+	}
 
 
 	static class ShaderIDs
@@ -63,6 +105,43 @@ public class VirtualProjector : MonoBehaviour
 	void OnValidate()
 	{
 		_dirty = true;
+
+		brightness = _brightness;
+		range = _range;
+		tint = _tint;
+	}
+
+
+	void OnDrawGizmos()
+	{
+		if( _drawGizmosAlways ) DrawGizmos();
+	}
+
+
+	void OnDrawGizmosSelected()
+	{
+		if( !_drawGizmosAlways ) DrawGizmos();
+	}
+
+
+	void DrawGizmos()
+	{
+		float imageWidthPx = _texture ? _texture.width : 1f;
+		float imageHeightPx = _texture ? _texture.height : 1f;
+		float aspect = imageWidthPx / imageHeightPx;
+		Vector3 corner = new Vector3( 0.5f / _throwRatio, 0.5f / aspect / _throwRatio, 1f );
+		Vector2 shift = new Vector2( corner.x * _horizontalLensShift, corner.y * _verticalLensShift ) * 2f;
+		Vector3 cornerUR = new Vector3( corner.x + shift.x, corner.y + shift.y, corner.z );
+		Vector3 cornerLR = new Vector3( corner.x + shift.x, -corner.y + shift.y, corner.z );
+		Vector3 cornerLL = new Vector3( -corner.x + shift.x, -corner.y + shift.y, corner.z );
+		Vector3 cornerUL = new Vector3( -corner.x + shift.x, corner.y + shift.y, corner.z );
+		
+		Gizmos.color = _gizmoColor;
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.DrawRay( Vector3.zero, cornerUR * _range );
+		Gizmos.DrawRay( Vector3.zero, cornerLR * _range );
+		Gizmos.DrawRay( Vector3.zero, cornerLL * _range );
+		Gizmos.DrawRay( Vector3.zero, cornerUL * _range );
 	}
 
 
@@ -73,10 +152,10 @@ public class VirtualProjector : MonoBehaviour
 
 		float imageWidthPx = _texture ? _texture.width : 1f;
 		float imageHeightPx = _texture ? _texture.height : 1f;
-		float aspect = imageWidthPx / imageHeightPx;
-		float diagonalExtents = Mathf.Sqrt( imageWidthPx * imageWidthPx + imageHeightPx * imageHeightPx ) * 0.5f;
-		float imageWidthProportion = 0.5f * imageWidthPx / diagonalExtents;
-		float imageHeightProportion = 0.5f * imageHeightPx / diagonalExtents;
+
+		float imageDiagonalExtents = Mathf.Sqrt( imageWidthPx * imageWidthPx + imageHeightPx * imageHeightPx ) * 0.5f;
+		float imageWidthProportion = 0.5f * imageWidthPx / imageDiagonalExtents;
+		float imageHeightProportion = 0.5f * imageHeightPx / imageDiagonalExtents;
 		float spotWidthProportion = 1f / imageWidthProportion;
 		float spotHeightProportion = 1f / imageHeightProportion;
 		float cokieSizePxF = imageWidthPx * spotWidthProportion;
@@ -88,6 +167,30 @@ public class VirtualProjector : MonoBehaviour
 			-( 1f - imageWidthProportion ) * 0.5f * spotWidthProportion,
 			-( 1f - imageHeightProportion ) * 0.5f * spotHeightProportion
 		);
+
+		// TODO.
+		float horizontalShiftProportion = ( 1f + _horizontalLensShift * 2f );
+		float verticalShiftProportion = ( 1f + _verticalLensShift * 2f );
+		float shiftedImageAreaWidthPx = imageWidthPx * horizontalShiftProportion; // We need to extend in both directions.
+		float shiftedImageAreaHeightPx = imageHeightPx * verticalShiftProportion;
+		float shiftedImageAreaDiagonalExtents = Mathf.Sqrt( shiftedImageAreaWidthPx * shiftedImageAreaWidthPx + shiftedImageAreaHeightPx * shiftedImageAreaHeightPx ) * 0.5f;
+		float shiftedImageAreaWidthProportion = 0.5f * imageWidthPx / shiftedImageAreaDiagonalExtents;
+		float shiftedImageAreaHeightProportion = 0.5f * imageHeightPx / shiftedImageAreaDiagonalExtents;
+		float shiftedSpotWidthProportion = 1f / shiftedImageAreaWidthProportion;
+		float shiftedSpotHeightProportion = 1f / shiftedImageAreaHeightProportion;
+		float shiftedCokieSizePxF = imageWidthPx * shiftedSpotWidthProportion;
+		int shiftedCokieSizePx = Mathf.CeilToInt( shiftedCokieSizePxF );
+		float shiftedSpotAngle = Mathf.Atan2( 0.5f * shiftedSpotWidthProportion / _throwRatio, 1f ) * Mathf.Rad2Deg * 2f;
+		var shiftedImageTransform = new Vector4(
+			horizontalShiftProportion + spotWidthProportion,
+			verticalShiftProportion + spotHeightProportion,
+			-( 1f - imageWidthProportion ) * 0.5f * spotWidthProportion,
+			-( 1f - imageHeightProportion ) * 0.5f * spotHeightProportion
+		);
+
+		//spotAngle = shiftedSpotAngle;
+		//imageTransform = shiftedImageTransform;
+
 
 		// Update light.
 		_light.spotAngle = spotAngle;
@@ -115,6 +218,11 @@ public class VirtualProjector : MonoBehaviour
 
 		// Apply. This seems to be necesarry to update the rendered cookie.
 		_light.cookie = _cookieTexture;
+
+		// Other lights settings.
+		brightness = _brightness;
+		range = _range;
+		tint = _tint;
 	}
 
 
@@ -132,6 +240,9 @@ public class VirtualProjector : MonoBehaviour
 			_light.transform.localPosition = Vector3.zero;
 			_light.transform.localRotation = Quaternion.identity;
 			_light.type = LightType.Spot;
+			_light.shadows = LightShadows.Hard;
+			//_light.shadowStrength = 1;
+			_dirty = true;
 		}
 		
 		if( !_blitMaterial ){
